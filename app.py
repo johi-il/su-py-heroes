b4bcb89f473708a27623dbd51ea3d2561989d89c
lewis wanjohi <i.johilewis@gmail.com>
2026-01-03 16:50:13 +0300

    initialization and configaration of flask app

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///heroes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+
